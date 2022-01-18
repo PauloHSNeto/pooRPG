0b4e3e09a61bf0d4be2384a51ad1f75e5034c55e
neto.564 <2100098@aluno.univesp.br>
2022-01-18 18:52:08 -0300

    new

--- Program.cs
+++ Program.cs
@@ -32,7 +32,7 @@ int mage_wins = 0;
 int bandit_wins = 0;
 List<string> winnertypes = new List<string>();
 int tourney = 0;
-while (tourney <12){
+while (tourney <120){
 Console.WriteLine("Hello RPG BattleRoyalle World!");
 Worrier warrior = new Worrier(names[rnd.Next(names.Count)], rnd.Next(25,35), "Worrier", rnd.Next(120,170), 15);
 Maegi mage = new Maegi(names[rnd.Next(names.Count)], rnd.Next(25,35), "Maegi",rnd.Next(80,100), 1);
